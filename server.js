d775f105c4d6a5897b9af5fa4bed039fcd1f91cc
aliasad466 <masood.aliasad@gmail.com>
2021-12-29 11:08:38 +0100

    multitenant cap saas app

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const cds = require("@sap/cds");
 
 cds.on("bootstrap", async (app) => {
 	await cds.mtx.in(app); // serve cds-mtx APIs
-
-	console.log("🤷‍♂️ Overriding Default Provisioning... ");
 	const provisioning = await cds.connect.to("ProvisioningService");
 	provisioning.impl(require("./srv/saas-provisioning/provisioning"));
 });
--- srv/saas-provisioning/provisioning.js
+++ srv/saas-provisioning/provisioning.js
@@ -1,8 +1,28 @@
 module.exports = (service) => {
+	
 	service.on("UPDATE", "tenant", async (req, next) => {
 		const res = await next(); // first call default implementation which is doing the HDI container creation
-		let url = `https://${req.data.subscribedSubdomain}.dt.cpp.cfapps.eu10.hana.ondemand.com/`;
+		let url = `https://${req.data.subscribedSubdomain}-portall-mtt-approuter.cfapps.eu10.hana.ondemand.com`;
 		console.log("[INFO ][ON_UPDATE_TENANT] " + "Application URL is " + url);
 		return url;
 	});
+
+	service.on( "dependencies", (req) => {
+		console.log("dependency will run or not???");
+		const xsenv = require("@sap/xsenv");
+		const services = xsenv.getServices({
+			portal: {
+				tag: "portal"
+			}
+		});
+
+		const dependencies = [{
+			xsappname: services.portal.uaa.xsappname
+		}];
+		//console.log("porta name is " + services.portal.uaa.xsappname);
+		//console.log("dependency are" +  JSON.stringify(dependencies));
+		
+		return dependencies;
+	});
+
 };
